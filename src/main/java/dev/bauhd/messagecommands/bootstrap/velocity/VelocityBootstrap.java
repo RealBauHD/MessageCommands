a9a1a06a44a1b2e5dd8c1fa0850a74ddfd8d830b
Aaron <bauhd@gmx.net>
2024-04-03 01:29:05 +0200

    chore: do not use legacy for velocity

--- src/main/java/dev/bauhd/messagecommands/bootstrap/velocity/VelocityBootstrap.java
+++ src/main/java/dev/bauhd/messagecommands/bootstrap/velocity/VelocityBootstrap.java
@@ -6,12 +6,17 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.PluginDescription;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import dev.bauhd.messagecommands.MessageCommands;
+import dev.bauhd.messagecommands.utils.MessageCommand;
 import java.io.File;
-import java.util.Objects;
-import java.util.logging.Logger;
+import java.nio.file.Path;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 @Plugin(
     id = "messagecommands",
@@ -24,44 +29,62 @@ import net.kyori.adventure.text.Component;
 public class VelocityBootstrap {
 
   private final ProxyServer proxyServer;
-  private final Logger logger;
+  private final ComponentLogger logger;
+  private final PluginDescription description;
+  private final Path dataDirectory;
 
-  private MessageCommands messageCommands;
+  private Component prefix;
 
   @Inject
-  public VelocityBootstrap(final ProxyServer proxyServer, final Logger logger) {
+  public VelocityBootstrap(
+      final ProxyServer proxyServer,
+      final ComponentLogger logger,
+      final PluginDescription description,
+      final @DataDirectory Path dataDirectory
+  ) {
     this.proxyServer = proxyServer;
     this.logger = logger;
+    this.description = description;
+    this.dataDirectory = dataDirectory;
   }
 
   @Subscribe
   public void handle(final ProxyInitializeEvent event) {
-    final File dataFolder = new File("plugins/MessageCommands");
+    final File dataFolder = this.dataDirectory.toFile();
     dataFolder.mkdir();
-    this.messageCommands = new MessageCommands(new File(dataFolder, "config.json"));
-    final String prefix = this.messageCommands.getConfig().getPrefix();
+    final MessageCommands messageCommands = new MessageCommands(
+        new File(dataFolder, "config.json"));
+    final LegacyComponentSerializer serializer = LegacyComponentSerializer.legacySection();
+    this.prefix = serializer.deserialize(messageCommands.getConfig().getPrefix());
 
-    this.messageCommands.getConfig().getMessageCommands().forEach(messageCommand -> {
-      if (!messageCommand.isEnabled()) {
-        return;
+    for (final MessageCommand command : messageCommands.getConfig().getMessageCommands()) {
+      if (!command.isEnabled()) {
+        continue;
       }
 
-      this.proxyServer.getCommandManager().register(messageCommand.getName(),
-          (SimpleCommand) invocation -> invocation.source()
-              .sendMessage(Component.text(prefix + messageCommand.getMessage())),
-          messageCommand.getAliases());
-    });
+      this.proxyServer.getCommandManager().register(command.getName(),
+          (SimpleCommand) invocation -> invocation.source().sendMessage(
+              Component.textOfChildren(prefix, serializer.deserialize(command.getMessage()))),
+          command.getAliases());
+    }
 
-    this.logger.info(prefix + "§aThe plugin MessageCommands has been activated.");
-    this.logger.info(prefix + "§7Developer: §bBauHD");
-    this.logger.info(
-        prefix + "§7Version: §b" + Objects.requireNonNull(this.proxyServer.getPluginManager()
-            .getPlugin("messagecommands").orElse(null)).getDescription().getVersion());
+    this.logger.info(Component.textOfChildren(
+        this.prefix,
+        Component.text("The plugin MessageCommands has been activated.", NamedTextColor.GREEN),
+        Component.newline(),
+        this.prefix,
+        Component.text("Developer: ", NamedTextColor.GRAY),
+        Component.text("BauHD", NamedTextColor.AQUA),
+        Component.newline(),
+        this.prefix,
+        Component.text("Version: ", NamedTextColor.GRAY),
+        Component.text(this.description.getVersion().orElse(""), NamedTextColor.AQUA)
+    ));
   }
 
   @Subscribe
   public void handle(final ProxyShutdownEvent event) {
-    this.logger.info(this.messageCommands.getConfig().getPrefix()
-        + "§cThe plugin MessageCommands has been deactivated.");
+    this.logger.info(Component.textOfChildren(this.prefix,
+        Component.text("The plugin MessageCommands has been deactivated.", NamedTextColor.RED)));
   }
 }
